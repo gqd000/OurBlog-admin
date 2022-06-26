a56c717708d27bb13a3082edc3e713ab3506d4f9
bluehyssopu <1767807919@qq.com>
2022-06-26 15:40:52 +0800

    增加首页饼图和标签云

--- vue.config.js
+++ vue.config.js
@@ -8,8 +8,8 @@ module.exports = {
         // 例如将'localhost:8080/apix'代理到'www.example.com/api/xxx'
         proxy: {
             '/api': { // 匹配所有以 '/api'开头的请求路径
-                // target: 'http://138.2.94.113:8081', // 代理目标的基础路径
-                target: 'http://localhost:8081', // 代理目标的基础路径
+                target: 'http://138.2.94.113:8081', // 代理目标的基础路径
+                // target: 'http://localhost:8081', // 代理目标的基础路径
                 // secure: false,  // 如果是https接口，需要配置这个参数
                 // changeOrigin: true, // 支持跨域
                 // ws: false,
